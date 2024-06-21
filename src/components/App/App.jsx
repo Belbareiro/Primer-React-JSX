cc7ff2739364a85ef46bba96d835e1393c2f778b
Belbareiro <mbareiromereles@gmail.com>
2024-06-20 23:17:36 -0400

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      <h1>Hola Dojo!</h1>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
